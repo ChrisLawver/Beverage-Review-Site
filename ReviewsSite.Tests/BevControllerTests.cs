739e2bd799f28704457df5debad5edf5a90dd51b
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-02-18 13:05:11 -0500

    Added Bev Class and test

--- ReviewsSite.Tests/BevControllerTests.cs
+++ ReviewsSite.Tests/BevControllerTests.cs
@@ -2,21 +2,42 @@ using System;
 using System.Web.Mvc;
 using Xunit;
 using ReviewsSite.Controllers;
+using ReviewsSite.Models;
 
 namespace ReviewsSite.Tests
 {
     public class BevControllerTests
     {
-            [Fact]
-            public void Index_Should_Return_View()
-            {
-                BevController sut = new BevController();
 
+        BevController sut = new BevController();
+
+        [Fact]
+        public void Index_Should_Return_View()
+        {
+
+
+            var result = sut.Index();
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+
+        [Fact]
+        public void Index_Returns_BevModel_To_View()
+         {
+            //Arrange
+            Bev bev = new Bev();
+
+
+
+            //Act
                 var result = sut.Index();
 
-                Assert.IsType<ViewResult>(result);
 
-            }
-        
+            // Assert
+            Assert.IsType<Bev>(result.Model);
+            
+         }
+
     }
 }
